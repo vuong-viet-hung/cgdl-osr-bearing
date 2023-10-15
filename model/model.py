f4ca44602beea06091442c1b4da36dd4361b8972
vuong-viet-hung <vuongviethung156@gmail.com>
2023-10-15 12:38:37 +0700

    Create model/model.py

---
+++ model/model.py
@@ -0,0 +1,25 @@
+import torch
+
+
+def build_conv(
+    in_channels: int,
+    out_channels: int,
+    kernel_size: int | tuple,
+    stride: int | tuple,
+    padding: int | tuple,
+) -> torch.nn.Sequential:
+    return torch.nn.Sequential(
+
+    )
+
+
+def build_tconv(
+    in_channels: int,
+    out_channels: int,
+    kernel_size: int | tuple,
+    stride: int | tuple,
+    padding: int | tuple,
+) -> torch.nn.Sequential:
+    return torch.nn.Sequential(
+
+    )
