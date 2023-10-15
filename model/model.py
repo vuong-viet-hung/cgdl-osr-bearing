4683e1334c77723170f0cdb2b121d5c7276ede33
vuong-viet-hung <vuongviethung156@gmail.com>
2023-10-15 21:40:58 +0700

    Update upsample mode to bilinear

--- model/model.py
+++ model/model.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 import torch
 
 
@@ -7,80 +5,84 @@ class VAE(torch.nn.Module):
 
     def __init__(self, in_channels: int, latent_dim: int) -> None:
         super().__init__()
-        self.encoder = encoder(in_channels)
-        self.mean = torch.nn.Linear(64 * 16 * 16, latent_dim)
-        self.var = torch.nn.Linear(64 * 16 * 16, latent_dim)
-        self.decoder = decoder(latent_dim, in_channels)
+        self.encoder = Encoder(in_channels, latent_dim)
+        self.decoder = Decoder(latent_dim, in_channels)
+
+    def forward(
+        self, inputs: torch.FloatTensor
+    ) -> tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
+        mean, var = self.encoder(inputs)
+        eps = torch.randn_like(var)
+        std = var.sqrt()
+        latent = mean + eps * std
+        outputs = self.decoder(latent)
+        return outputs, mean, var
+
+
+class Encoder(torch.nn.Module):
+
+    def __init__(self, in_channels: int, latent_dim: int) -> None:
+        super().__init__()
+        self.conv1_1 = Conv3x3(in_channels, out_channels=32)
+        self.conv1_2 = Conv3x3(in_channels=32, out_channels=32)
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2)
+        self.conv2_1 = Conv3x3(in_channels=32, out_channels=64)
+        self.conv2_2 = Conv3x3(in_channels=64, out_channels=64)
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=2)
+        self.flatten = torch.nn.Flatten()
+        self.fc1 = torch.nn.Linear(64 * 8 * 8, latent_dim)
+        self.fc2 = torch.nn.Linear(64 * 8 * 8, latent_dim)
+
+    def forward(
+        self, inputs: torch.FloatTensor
+    ) -> tuple[torch.FloatTensor, torch.FloatTensor]:
+        conv1_1 = self.conv1_1(inputs)
+        conv1_2 = self.conv1_2(conv1_1)
+        pool1 = self.pool1(conv1_2)
+        conv2_1 = self.conv2_1(pool1)
+        conv2_2 = self.conv2_2(conv2_1)
+        pool2 = self.pool2(conv2_2)
+        flatten = self.flatten(pool2)
+        mean = self.fc1(flatten)
+        var = self.fc2(flatten)
+        return mean, var
+
+
+class Decoder(torch.nn.Module):
+
+    def __init__(self, latent_dim: int, out_channels: int) -> None:
+        super().__init__()
+        self.fc = torch.nn.Linear(latent_dim, 64 * 8 * 8)
+        self.unflatten = torch.nn.Unflatten(dim=1, unflattened_size=(64, 8, 8))
+        self.upsample1 = torch.nn.Upsample(scale_factor=2, mode="bilinear")
+        self.deconv1_1 = Conv3x3(in_channels=64, out_channels=32)
+        self.deconv1_2 = Conv3x3(in_channels=32, out_channels=32)
+        self.upsample2 = torch.nn.Upsample(scale_factor=2, mode="bilinear")
+        self.deconv2_1 = Conv3x3(in_channels=32, out_channels=3)
+        self.deconv2_2 = torch.nn.Conv2d(
+            in_channels=3, out_channels=out_channels, kernel_size=3, padding=1
+        )
 
     def forward(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
-        enc_outputs = self.encoder(inputs)
-        mean = self.mean(enc_outputs)
-        var = self.var(enc_outputs)
-        latent = sample_gaussian(mean, var)
-        outputs = self.decoder(latent)
-        return outputs
+        fc = self.fc(inputs)
+        unflatten = self.unflatten(fc)
+        upsample1 = self.upsample1(unflatten)
+        deconv1_1 = self.deconv1_1(upsample1)
+        deconv1_2 = self.deconv1_2(deconv1_1)
+        upsample2 = self.upsample2(deconv1_2)
+        deconv2_1 = self.deconv2_1(upsample2)
+        deconv2_2 = self.deconv2_2(deconv2_1)
+        return deconv2_2
 
 
-def encoder(in_channels: int) -> torch.nn.Sequential:
-    conv1_1 = conv3x3(in_channels, out_channels=32)
-    conv1_2 = conv3x3(in_channels=32, out_channels=32)
-    pool1 = torch.nn.MaxPool2d(kernel_size=2)
-    conv2_1 = conv3x3(in_channels=32, out_channels=64)
-    conv2_2 = conv3x3(in_channels=64, out_channels=64)
-    pool2 = torch.nn.MaxPool2d(kernel_size=2)
-    flatten = torch.nn.Flatten()
-    return torch.nn.Sequential(
-        OrderedDict(
-            [
-                ("conv1_1", conv1_1),
-                ("conv1_2", conv1_2),
-                ("pool1", pool1),
-                ("conv2_1", conv2_1),
-                ("conv2_2", conv2_2),
-                ("pool2", pool2),
-                ("flatten", flatten),
-            ]
+class Conv3x3(torch.nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super().__init__()
+        self.conv = torch.nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, padding=1
         )
-    )
+        self.relu = torch.nn.ReLU()
 
-
-def decoder(latent_dim: int, out_channels: int) -> torch.nn.Sequential:
-    fc = torch.nn.Linear(latent_dim, 64 * 16 * 16)
-    unflatten = torch.nn.Unflatten(dim=1, unflattened_size=(64, 16, 16))
-    upsample1 = torch.nn.Upsample(scale_factor=2)
-    conv1_1 = conv3x3(in_channels=64, out_channels=32)
-    conv1_2 = conv3x3(in_channels=32, out_channels=32)
-    upsample2 = torch.nn.Upsample(scale_factor=2)
-    conv2_1 = conv3x3(in_channels=32, out_channels=3)
-    conv2_2 = conv3x3(in_channels=3, out_channels=out_channels)
-    return torch.nn.Sequential(
-        OrderedDict(
-            [
-                ("fc", fc),
-                ("unflatten", unflatten),
-                ("upsample1", upsample1),
-                ("conv1_1", conv1_1),
-                ("conv1_2", conv1_2),
-                ("upsample2", upsample2),
-                ("conv2_1", conv2_1),
-                ("conv2_2", conv2_2),
-            ]
-        )
-    )
-
-
-@torch.no_grad()
-def sample_gaussian(
-    mean: torch.FloatTensor, var: torch.FloatTensor
-) -> torch.FloatTensor:
-    eps = torch.randn(mean.shape)
-    std = var.sqrt()
-    return mean + eps * std
-
-
-def conv3x3(in_channels: int, out_channels: int) -> torch.nn.Sequential:
-    conv = torch.nn.Conv2d(
-        in_channels, out_channels, kernel_size=3, padding=1
-    )
-    relu = torch.nn.ReLU()
-    return torch.nn.Sequential(OrderedDict([("conv", conv), ("relu", relu)]))
+    def forward(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
+        return self.relu(self.conv(inputs))
---
+++ train.py
@@ -0,0 +1,22 @@
+import torch
+
+from argparse import ArgumentParser
+
+from model import VAE
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    args = parser.parse_args()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = VAE(in_channels=1, latent_dim=10).to(device)
+    print(model)
+    with torch.no_grad():
+        inputs = torch.zeros(1, 1, 32, 32, device=device)
+        outputs, mean, var = model(inputs)
+        print(outputs.shape)
+
+
+if __name__ == "__main__":
+    main()
