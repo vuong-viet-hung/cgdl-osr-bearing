f6da1ca9cdeb36093ef7daa77b8acf7bf5cf4e1b
vuong-viet-hung <vuongviethung156@gmail.com>
2023-10-16 14:43:32 +0700

    Use ReLU as default activation

--- model/model.py
+++ model/model.py
@@ -12,25 +12,24 @@ class VAE(torch.nn.Module):
         self, inputs: torch.FloatTensor
     ) -> tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
         mean, var = self.encoder(inputs)
-        std = var.sqrt()
-        latent = torch.normal(mean, std)
+        latent = random_sample(mean, var)
         outputs = self.decoder(latent)
         return outputs, mean, var
 
 
 class Encoder(torch.nn.Module):
 
-    def __init__(self, in_channels: int, latent_dim: int) -> None:
+    def __init__(self, in_channels: int, out_features: int) -> None:
         super().__init__()
-        self.conv1_1 = Conv3x3(in_channels, out_channels=32)
-        self.conv1_2 = Conv3x3(in_channels=32, out_channels=32)
+        self.conv1_1 = ConvReLU(in_channels, out_channels=32)
+        self.conv1_2 = ConvReLU(in_channels=32, out_channels=32)
         self.pool1 = torch.nn.MaxPool2d(kernel_size=2)
-        self.conv2_1 = Conv3x3(in_channels=32, out_channels=64)
-        self.conv2_2 = Conv3x3(in_channels=64, out_channels=64)
+        self.conv2_1 = ConvReLU(in_channels=32, out_channels=64)
+        self.conv2_2 = ConvReLU(in_channels=64, out_channels=64)
         self.pool2 = torch.nn.MaxPool2d(kernel_size=2)
         self.flatten = torch.nn.Flatten()
-        self.fc1 = torch.nn.Linear(64 * 8 * 8, latent_dim)
-        self.fc2 = torch.nn.Linear(64 * 8 * 8, latent_dim)
+        self.fc1 = torch.nn.Linear(64 * 8 * 8, out_features)
+        self.fc2 = torch.nn.Linear(64 * 8 * 8, out_features)
 
     def forward(
         self, inputs: torch.FloatTensor
@@ -49,19 +48,16 @@ class Encoder(torch.nn.Module):
 
 class Decoder(torch.nn.Module):
 
-    def __init__(self, latent_dim: int, out_channels: int) -> None:
+    def __init__(self, in_features: int, out_channels: int) -> None:
         super().__init__()
-        self.fc = torch.nn.Linear(latent_dim, 64 * 8 * 8)
+        self.fc = torch.nn.Linear(in_features, 64 * 8 * 8)
         self.unflatten = torch.nn.Unflatten(dim=1, unflattened_size=(64, 8, 8))
         self.upsample1 = torch.nn.Upsample(scale_factor=2, mode="bilinear")
-        self.deconv1_1 = Conv3x3(in_channels=64, out_channels=32)
-        self.deconv1_2 = Conv3x3(in_channels=32, out_channels=32)
+        self.deconv1_1 = ConvReLU(in_channels=64, out_channels=32)
+        self.deconv1_2 = ConvReLU(in_channels=32, out_channels=32)
         self.upsample2 = torch.nn.Upsample(scale_factor=2, mode="bilinear")
-        self.deconv2_1 = Conv3x3(in_channels=32, out_channels=3)
-        activation = torch.nn.Sigmoid()
-        self.deconv2_2 = Conv3x3(
-            in_channels=3, out_channels=out_channels, activation=activation
-        )
+        self.deconv2_1 = ConvReLU(in_channels=32, out_channels=3)
+        self.deconv2_2 = ConvSigmoid(in_channels=3, out_channels=out_channels)
 
     def forward(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
         fc = self.fc(inputs)
@@ -75,21 +71,36 @@ class Decoder(torch.nn.Module):
         return deconv2_2
 
 
-class Conv3x3(torch.nn.Module):
+class ConvReLU(torch.nn.Module):
 
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        activation: torch.nn.Module | None = None
-    ) -> None:
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.conv = torch.nn.Conv2d(
             in_channels, out_channels, kernel_size=3, padding=1
         )
-        self.activation = (
-            torch.nn.ReLU() if activation is None else activation
-        )
+        self.relu = torch.nn.ReLU()
 
     def forward(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
-        return self.activation(self.conv(inputs))
+        return self.relu(self.conv(inputs))
+
+
+class ConvSigmoid(torch.nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super().__init__()
+        self.conv = torch.nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, padding=1
+        )
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
+        return self.sigmoid(self.conv(inputs))
+
+
+@torch.no_grad()
+def random_sample(
+    mean: torch.FloatTensor, var: torch.FloatTensor
+) -> torch.FloatTensor:
+    std = var.sqrt()
+    latent = torch.normal(mean, std)
+    return latent  # type: ignore
